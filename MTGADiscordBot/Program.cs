dc062dc972550a4f75b38be8af0ff8a89fd4b679
jamalstevens <21340653+JamalStevens@users.noreply.github.com>
2020-07-08 17:09:30 -0400

    Add project files.

---
+++ MTGADiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MTGADiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
